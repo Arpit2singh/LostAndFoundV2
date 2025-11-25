96b22051c860ee994038b48a9479ef60a22e3c86
Arpit Singh <arpitlibono57@gmail.com>
2025-11-25 23:55:30 +0530

    speed adjusted

--- src/components/ProLib.jsx
+++ src/components/ProLib.jsx
@@ -15,7 +15,7 @@ const ProLib = () => {
                             lightSpread={0.8}
                             rayLength={1.2}
                             followMouse={true}
-                            mouseInfluence={0.1}
+                            mouseInfluence={1}
                             noiseAmount={0.1}
                             distortion={0.05}
                             className="custom-rays"
